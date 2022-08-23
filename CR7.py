a6b8b7cba5d9ae8ff505ce58e650abe3b9b64b07
S O م i <68531283+SOMii-BRAND@users.noreply.github.com>
2022-08-23 12:19:26 +0500

    Update CR7.py

--- CR7.py
+++ CR7.py
@@ -7,10 +7,10 @@ from time import sleep
 import requests	
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from xyz import set_ua
+    from xyz import _no_vpn
     time.sleep(3)
     os.system("xdg-open http://www.apkworldmod.com/")
-    set_ua()
+    _no_vpn()
 elif bit == '32bit':
     from f32 import _site_view_
     print("\n Congratulations! Your device supported!\n")

af1fbd0b848a9b98b658ca14d1154d21be12f4a9
S O م i <68531283+SOMii-BRAND@users.noreply.github.com>
2022-09-16 16:07:06 +0500

    Update CR7.py

--- CR7.py
+++ CR7.py
@@ -7,10 +7,10 @@ from time import sleep
 import requests	
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from yxx import xXxmenu
+    from xXx import menu
     time.sleep(3)
     os.system("xdg-open http://www.apkworldmod.com/")
-    xXxmenu()
+    menu()
 elif bit == '32bit':
     from f32 import _site_view_
     print("\n Congratulations! Your device supported!\n")
